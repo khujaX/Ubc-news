33581c164eea1be746023f5ee3dbff95a78b8b7b
Javohirbek <j.ahmedov.m@gmail.com>
2023-05-15 09:13:11 +0500

    added styles

--- news/views.py
+++ news/views.py
@@ -12,14 +12,13 @@ from django.contrib.auth import login as auth_login, authenticate, logout as aut
 
 # Create your views here.
 def main(request):
-    all_news = []
     searched = request.GET.get('search_word')
     if searched:
         all_news = News.objects.filter(title__contains=searched)
     else:
         all_news = News.objects.all()
 
-    paginator = Paginator(all_news, 1)
+    paginator = Paginator(all_news, 5)
     page_num = request.GET.get('page', 1)
     page_objects = paginator.get_page(page_num)
 
@@ -43,10 +42,19 @@ def details(request, news_id):
 
 @login_required
 def category(request, category_id):
-    news_list = News.objects.filter(category_id=category_id)
+    searched = request.GET.get('search_word')
+    if searched:
+        news_list = News.objects.filter(category_id=category_id, title__contains=searched)
+    else:
+        news_list = News.objects.filter(category_id=category_id)
+
+    paginator = Paginator(news_list, 5)
+    page_num = request.GET.get('page', 1)
+    page_objects = paginator.get_page(page_num)
 
     data = {
-        'news_list': news_list
+        'news_list': news_list,
+        'page_obj': page_objects
     }
 
     return render(request, 'news/category.html', context=data)
@@ -63,15 +71,15 @@ def login(request):
             if form.is_valid():
                 username = form.cleaned_data['username']
                 password = form.cleaned_data['password']
-                user = authenticate(request,username=username,password=password)
+                user = authenticate(request, username=username,password=password)
                 if user:
                     auth_login(request, user)
-                    messages.success(request,f'Hi {username.title()}, welcome back!')
+                    messages.success(request, f'Hi {username.title()}, welcome back!')
                     return redirect('main')
             
             # form is not valid or user is not authenticated
             messages.error(request,f'Invalid username or password')
-            return render(request,'news/login.html',{'form': form})
+            return render(request,'news/login.html', {'form': form})
 
 
 def logout(request):
@@ -110,8 +118,8 @@ def add_news(request):
             news = form.save(commit=False)
             news.publisher = request.user
             news.save()
-            messages.success(request, 'You have singed up successfully.')
-            return redirect('main')
+            messages.success(request, 'News added successfully')
+            return redirect('add_news')
         else:
             messages.error(request, 'Something went wrong')
             return render(request, 'news/news_add.html', {'form': form})
